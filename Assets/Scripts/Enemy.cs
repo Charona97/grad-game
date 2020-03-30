afa101712f88cacef5596ad9f94ef2f4613fd4d8
charona <c.t.poeran@gmail.com>
2020-03-30 10:13:42 +0200

    inserted enemies patrolling

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,14 @@ using Pathfinding;
 public class Enemy : MonoBehaviour
 {
     public int health = 2;
-    private AIPath aiPath;
+   
     private Movement player;
-    public GameObject lootDrop;
+    
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
-        aiPath = GetComponent<AIPath>();
+        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -27,19 +27,12 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (aiPath.desiredVelocity.x >= 0.01f)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-
-        } else if (aiPath.desiredVelocity.x <= -0.01f)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
+        
 
         if (health <= 0)
         {
             Destroy(gameObject);
-            Instantiate(lootDrop, transform.position, Quaternion.identity);
+            
         }
         
     }
